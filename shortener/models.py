4038ea9290a1417b7380b15c2fdc6ad0458cc7c5
omniaura <omniaura5@gmail.com>
2013-12-19 13:38:03 -0700

    models

--- shortener/models.py
+++ shortener/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Adjective(models.Model):
+    word = models.CharField(max_length=16)
+
+
+class Noun(models.Model):
+    word = models.CharField(max_length=16)
+
+
+class Link(models.Model):
+    adjective = models.ForeignKey('Adjective')
+    noun = models.ForeignKey('Noun')
+    target = models.URLField() 
+        # default max_length=200; may need to be increased
