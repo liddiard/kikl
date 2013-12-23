4e2469c66e2aeb3f8137b60432700c3a336e81e3
omniaura <omniaura5@gmail.com>
2013-12-23 09:43:26 -0700

    AjaxView methods

--- shortener/views.py
+++ shortener/views.py
@@ -26,8 +26,40 @@ class AboutPageView(TemplateView):
 # api
 
 class AjaxView(View):
-    pass
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.is_ajax():
+            return super(AjaxView, self).dispatch(request, *args, **kwargs)
+        else:
+            raise Http404
+
+    def json_response(self, **kwargs):
+        return HttpResponse(json.dumps(kwargs), content_type="application/json")
+
+    def success(self, **kwargs):
+        return self.json_response(result=0, **kwargs)
+
+    def error(self, error_type, message):
+        return self.json_response(result=1, error=error_type, message=message)
+
+    def authentication_error(self):
+        return self.error("AuthenticationError", "User is not authenticated.")
+
+    def key_error(self, message):
+        return self.error("KeyError", message)
+
+    def does_not_exist(self, message):
+        return self.error("DoesNotExist", message)
+
+    def validation_error(self, message):
+        return self.error("ValidationError", message)
 
 
 class AuthenticatedAjaxView(AjaxView):
-    pass
+    
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated():
+            return super(AuthenticatedAjaxView, self).dispatch(request, *args,
+                                                               **kwargs)
+        else:
+            return self.authentication_error()
