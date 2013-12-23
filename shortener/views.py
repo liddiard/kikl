f39b6452441c0cabd7b356542ec2cdd04d61558e
omniaura <omniaura5@gmail.com>
2013-12-23 01:52:37 -0700

    class-based views skeleton

--- shortener/views.py
+++ shortener/views.py
@@ -4,6 +4,30 @@ from django.views.generic.base import View, TemplateView
 from shortener import models
 
 
+# pages
+
 class FrontPageView(TemplateView):
     
     template_name = "front.html"
+
+
+class LinkView(DetailView):
+    pass
+
+
+class LinksView(ListView):
+    pass
+
+
+class AboutPageView(TemplateView):
+    pass
+
+
+# api
+
+class AjaxView(View):
+    pass
+
+
+class AuthenticatedAjaxView(AjaxView):
+    pass
