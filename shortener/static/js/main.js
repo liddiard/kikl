e2ea51592be12b9d81a0956ab5765ca533953e8d
omniaura <omniaura5@gmail.com>
2013-12-23 01:46:35 -0700

    added main.js with utility functions

---
+++ shortener/static/js/main.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    ;
+});
+
+
+/* utility functions */
+
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+function ajaxPost(params, endpoint, callback_success) {
+    params.csrfmiddlewaretoken = getCookie('csrftoken');
+    $.ajax({
+        type: "POST",
+        url: endpoint,
+        data: params,
+        success: callback_success,
+        error: function(xhr, textStatus, errorThrown) {
+            console.log("Oh no! Something went wrong. Please report this error: \n"+errorThrown+xhr.status+xhr.responseText);
+        }
+    }); 
+}
