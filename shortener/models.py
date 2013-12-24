d48062f84405746efb9aafe9616a9fb64812613b
omniaura <omniaura5@gmail.com>
2013-12-24 16:50:53 -0700

    added fields to Link model, imported generic views in views.py

--- shortener/models.py
+++ shortener/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Adjective(models.Model):
@@ -14,3 +15,6 @@ class Link(models.Model):
     noun = models.ForeignKey('Noun')
     target = models.URLField() 
         # default max_length=200; may need to be increased
+    ip_added = models.IPAddressField()
+    user_added = models.ForeignKey(User, blank=True, null=True)
+    time_added = models.DateTimeField(auto_now_add=True)
--- shortener/views.py
+++ shortener/views.py
@@ -1,6 +1,8 @@
 import json
 from django.shortcuts import render
 from django.views.generic.base import View, TemplateView
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 from shortener import models
 
 
