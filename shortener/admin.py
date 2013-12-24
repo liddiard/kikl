332955d8f0f787f9a74e87b4255ec708897a3466
omniaura <omniaura5@gmail.com>
2013-12-24 16:53:38 -0700

    registered models in admin

--- shortener/admin.py
+++ shortener/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from shortener.models import Adjective, Noun, Link
 
-# Register your models here.
+
+class AdjectiveAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Adjective, AdjectiveAdmin)
+
+
+class NounAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Noun, NounAdmin)
+
+
+class LinkAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Link, LinkAdmin)
