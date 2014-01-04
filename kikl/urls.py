94e4cb4912eb17fea10ff787eb4aabfdd3eed841
omniaura <omniaura5@gmail.com>
2014-01-03 20:52:19 -0800

    changed urlconf ordering

--- kikl/urls.py
+++ kikl/urls.py
@@ -6,16 +6,19 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    # front
     url(r'^$', views.FrontPageView.as_view(), name='front'),
-    url(r'^(?P<adjective>\S+)-(?P<noun>\S+)/$', views.target_view, 
-        name='target'),
-    url(r'^(?P<adjective>\S+)-(?P<noun>\S+)/time/$', views.LinkView.as_view(), 
-        name='link'),
 
     # api
     url(r'^api/link-add/$', views.AddLinkView.as_view(), name='link_add'),
     url(r'^api/link-increaseduration/$', views.IncreaseDurationView.as_view(), 
         name='link_increaseduration'),
+
+    # main
+    url(r'^(?P<adjective>\S+)-(?P<noun>\S+)/$', views.target_view, 
+        name='target'),
+    url(r'^(?P<adjective>\S+)-(?P<noun>\S+)/time/$', views.LinkView.as_view(), 
+        name='link'),
 
     # admin
     url(r'^admin/', include(admin.site.urls)),
