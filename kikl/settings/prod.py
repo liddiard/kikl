42644c91e209a17a8e91ffecbbec13527bc5a9bb
omniaura <omniaura5@gmail.com>
2014-01-07 21:25:53 -0800

    production email settings

--- kikl/settings/prod.py
+++ kikl/settings/prod.py
@@ -6,3 +6,10 @@ DEBUG = False
 DATABASES = {
     'default': dj_database_url.config()
 }
+
+# email settings
+EMAIL_HOST = "smtp.mandrillapp.com"
+EMAIL_PORT = 587
+EMAIL_HOST_USER = get_env_variable('MANDRILL_USERNAME')
+EMAIL_HOST_PASSWORD = get_env_variable('MANDRILL_APIKEY')
+DEFAULT_FROM_EMAIL = "kikl.co <no-reply@kikl.co>"
