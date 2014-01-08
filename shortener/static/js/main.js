996a8da4b136506245a31c1a03ea1b055bf358ed
omniaura <omniaura5@gmail.com>
2014-01-07 23:10:56 -0800

    user menu spacing improvements, added 'Your active links' item to menu

--- shortener/static/js/main.js
+++ shortener/static/js/main.js
@@ -45,7 +45,7 @@ function ajaxAddLink(target) {
             var decorated_url = build_url(response.path, true);
             $('.link-url').html(decorated_url);
             var time_url = url+'/time';
-            $('.check-time-remaining').html('You can check the time remaining on this link anytime by going to <a href="'+time_url+'">'+time_url+'</a>.');
+            $('.check-time-remaining').html('You can check the time remaining on this link by going to <a href="'+time_url+'">'+time_url+'</a>.');
             $('button.add-another').show();
         }
         else { // there's an error
