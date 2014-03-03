564732323059a6cb5ddf56e7a1ea1afa0d3830db
omniaura <omniaura5@gmail.com>
2014-03-03 00:32:59 -0800

    set mandrill api key (changed var name), prod DEBUG OFF

--- kikl/settings/prod.py
+++ kikl/settings/prod.py
@@ -1,7 +1,7 @@
 from .base import *
 import dj_database_url
 
-DEBUG = True
+DEBUG = False
 
 
 DATABASES = {
@@ -9,6 +9,6 @@ DATABASES = {
 }
 
 # email settings
-EMAIL_HOST_USER = get_env_variable('MANDRILL_API_KEY')
+MANDRILL_API_KEY = get_env_variable('MANDRILL_API_KEY')
 EMAIL_BACKEND = "djrill.mail.backends.djrill.DjrillBackend"
 DEFAULT_FROM_EMAIL = "kikl.co <no-reply@kikl.co>"
