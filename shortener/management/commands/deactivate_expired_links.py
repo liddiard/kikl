ee2cdac68b30119b8d414f0e542d596f7e6bc57d
omniaura <omniaura5@gmail.com>
2014-03-13 00:17:24 -0700

    nope, that wasn't it. readding file

---
+++ shortener/management/commands/deactivate_expired_links.py
@@ -0,0 +1,10 @@
+from django.core.management.base import BaseCommand, CommandError
+from shortener.models import Link
+
+class Command(BaseCommand):
+
+    help = "Deactivate all currently active links that have expired."
+
+    def handle(self, *args, **options):
+        for link in Link.objects.filter(is_active=True):
+            link.deactivate_if_expired()
