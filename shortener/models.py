d01541414347a1c8c04084b1835350a45babc7bf
Harrison Liddiard <omniaura5@gmail.com>
2024-07-15 17:16:27 -0400

    remove unused imports

--- shortener/models.py
+++ shortener/models.py
@@ -5,8 +5,6 @@ from datetime import timedelta
 from django.utils import timezone
 from django.db import models
 from django.core.exceptions import ValidationError
-from django.contrib.auth.models import User
-from django.core import serializers
 
 
 class WordManager(models.Manager):
