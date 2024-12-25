a521dd2528bff40fe6a33326b12c998a8ba1fa12
Harrison Liddiard <omniaura5@gmail.com>
2024-12-25 10:38:20 -0700

    improve admin link views

--- shortener/admin.py
+++ shortener/admin.py
@@ -5,6 +5,15 @@ admin.site.register(Adjective)
 admin.site.register(Noun)
 
 class LinkAdmin(admin.ModelAdmin):
-    readonly_fields = ('uuid',)
+    list_display = ('path', 'target_truncated', 'time_added', 'is_active')
+    list_filter = ('is_active',)
+    ordering = ('-is_active',)
+    readonly_fields = ('uuid', 'time_added')
+
+    def target_truncated(self, obj):
+        max_len = 40
+        """Truncate the 'target' value to 40 characters if needed, appending an ellipsis."""
+        return (obj.target[:max_len] + '…') if len(obj.target) > max_len else obj.target
+    target_truncated.short_description = 'Target'
 
 admin.site.register(Link, LinkAdmin)
